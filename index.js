bf100f14407176e37af4af40bdfd45873191b32b
ShahanaMahfuzaShanta <shantamahfuza2017@gmail.com>
2022-09-05 23:18:41 +0600

    added heroku end point

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require("express");
+const bodyParser = require("body-parser");
 const fs = require('fs');
 
 require("dotenv").config();
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const routes = require('./routes/Routes')
-app.use('/', routes)
+const routes = require('./routes/Routes');
+app.use('/', routes);
+
+app.get('/hero', (req, res) => {
+    res.send('server is running');
+});
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`);
