b05be89d5888a3ef8d76459fdb99c82937b9266c
ShahanaMahfuzaShanta <shantamahfuza2017@gmail.com>
2022-09-05 22:57:45 +0600

    updated

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const fs = require('fs');
 
+require("dotenv").config();
 const app = express()
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,6 +12,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const routes = require('./routes/Routes')
 app.use('/', routes)
 
-app.listen(5000, ()=>{
-    console.log("listeniing at port:5000")
-}) 
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`);
+});
